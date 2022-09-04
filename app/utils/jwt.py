35b335278fc5d824cd39c239230565d613fa96af
darius <jonat496@gmail.com>
2022-09-04 21:24:03 +0700

    V.0.0.2

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -1,15 +1,17 @@
-from app.config.configs import settings
-from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from typing import Union, Any
+
 from jose import jwt
+from passlib.context import CryptContext
+
+from app.config.configs import settings
 
 JWT_SECRET_KEY = settings.Secret
 ALGORITHM = settings.Algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.EXPIRE_MINUTES
 REFRESH_TOKEN_EXPIRE_MINUTES = settings.REFRESH_TOKEN_EXPIRE_MINUTES
 JWT_REFRESH_SECRET_KEY = settings.REFRESH_SECRET_KEY
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+password_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
